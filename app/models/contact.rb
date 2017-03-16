052bd70938bfbd77f4487a79cd93e89e098bdd32
Gianpaolo DAmato <damatohg@gmail.com>
2017-03-16 15:12:37 +0000

    Added form Validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-    
+  validates :nombre, presence: true
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :comentarios, presence: true
 end
