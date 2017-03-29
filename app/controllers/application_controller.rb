1d3529467f1cf91e357b05d56abda355f0b26f10
Gianpaolo DAmato <damatohg@gmail.com>
2017-03-29 13:00:09 +0000

    Added controler for reset password, conditional sing up and log out buttons, bootstrap styles to devise forms password, registrations, sessions and log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :mailer_set_url_options
+  
+  private
+    def mailer_set_url_options
+      ActionMailer::Base.default_url_options[:host] = request.host_with_port
+    end
 end
