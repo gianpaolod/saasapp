68045b4546be46bb660c3d3ed61d72f616ce9af4
Gianpaolo DAmato <damatohg@gmail.com>
2017-07-13 19:21:37 +0000

    Relationships models and db

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  has_many :microposts, dependent: :destroy   
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:   "Relationship",
+                                  foreing_key:  "follower_id",
+                                  dependent:    :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  
+  has_many :passive_relationships, class_name:   "Relationship",
+                                   foreing_key:  "followed_id",
+                                   dependent:    :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   belongs_to :plan
   has_one :profile
   
@@ -12,6 +22,24 @@ class User < ApplicationRecord
   # See "following users" for full implementation.
   def feed
     Micropost.where("user_id = ?", id).includes(:user)
+  end
+  
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+    #active_relationships.create(followed_id: other_user.id)
+  end
+  
+  #  Unfollows a user.
+  def unfollows(other_user)
+    following.delete(other_user)
+    #active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+  
+  # Returns true if the current user is following then user.
+  def following?(other_user)
+    following.include?(other_user)
+    #!active_relationships.find_by(followed_id: other_user.id).nil?
   end
   
   attr_accessor :stripe_card_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712152052) do
+ActiveRecord::Schema.define(version: 20170713130929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,16 @@ ActiveRecord::Schema.define(version: 20170712152052) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
